7ad49135eb98ffe27597f7fd17de3a424040d192
Shyam Guthikonda <shyamguth@gmail.com>
2014-07-04 09:44:45 +0900

    Import.

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Dun: require('./lib/dun'),
+    Til: require('./lib/til'),
+    Min: require('./lib/min'),
+    Cel: require('./lib/cel'),
+    Pal: require('./lib/pal'),
+    Sol: require('./lib/sol')
+};
